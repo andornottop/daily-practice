8b40a33669e916ff43885ec491743675cdb845cf
andornottop <andornot888@163.com>
2022-06-28 09:24:12 +0800

    leetcode:二叉树最大深度

--- leetcode/src/maximumdepthofbinarytree/MainClass.java
+++ leetcode/src/maximumdepthofbinarytree/MainClass.java
@@ -2,12 +2,9 @@ package maximumdepthofbinarytree;
 
 import pojo.TreeNode;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 
 public class MainClass {
     public static TreeNode stringToTreeNode(String input) {
--- leetcode/src/maximumdepthofbinarytree/Solution.java
+++ leetcode/src/maximumdepthofbinarytree/Solution.java
@@ -4,14 +4,14 @@ import pojo.TreeNode;
 
 class Solution {
     public int maxDepth(TreeNode root) {
-        if (root == null){
+        if (root == null) {
             return 0;
         }
         // 前序遍历
-       int leftMaxDepth =  maxDepth(root.left);
+        int leftMaxDepth = maxDepth(root.left);
         // 中序遍历
-        int rightMaxDepth =  maxDepth(root.right);
+        int rightMaxDepth = maxDepth(root.right);
         // 后序遍历
-        return Math.max(leftMaxDepth,rightMaxDepth) + 1;
+        return Math.max(leftMaxDepth, rightMaxDepth) + 1;
     }
 }
