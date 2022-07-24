b3bfa28b8d7fc0e30ea36902a7bcfa17135d376a
andornottop <andornot888@163.com>
2022-07-24 23:35:48 +0800

    leetcode:环形链表II

--- leetcode/src/linkedlist/linkedlistcycleii/Solution.java
+++ leetcode/src/linkedlist/linkedlistcycleii/Solution.java
@@ -5,6 +5,21 @@ import pojo.ListNode;
 public class Solution {
     public ListNode detectCycle(ListNode head) {
 
+        ListNode quick = head;
+        ListNode slow = head;
+
+        while (quick != null && quick.next != null) {
+            quick = quick.next.next;
+            slow = slow.next;
+            if (quick == slow) {
+                ListNode p = head;
+                while (p != slow) {
+                    p = p.next;
+                    slow = slow.next;
+                }
+                return p;
+            }
+        }
         return null;
     }
 }
